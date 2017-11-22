1db522be7757f2398905936cbb7d21209a7ba7e5
nusendra <admin@nusendra.com>
2017-11-22 14:09:09 +0700

    add 404 dan TDD

---
+++ tests/Unit/CRUDKategoriTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Validator;
+use App\Kategori;
+
+class KategoriRule {
+  public static $rules = [
+    'kategori' => 'required'
+  ];
+}
+
+class CRUDKategoriTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testCreate()
+    {
+      $val = Validator::make([
+          'kategori' => 'Islam'
+      ],KategoriRule::$rules);
+
+      if($val->passes()){
+        $kategori = Kategori::updateOrCreate(
+          ['id' => 0],
+          [
+            'kategori'        => 'Islam',
+            'created_by'      => 1
+          ]
+        );
+        $status = 1;
+      }
+      $this->assertDatabaseHas('kategoris',[
+        'kategori' => 'Islam'
+      ]);
+
+
+    }
+}
