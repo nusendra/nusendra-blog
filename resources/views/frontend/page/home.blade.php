07fcbc7a4220fb233d1efbcf554a6ad27c4344b0
nusendra <admin@nusendra.com>
2017-11-27 23:25:32 +0700

    tambahan dikit

--- resources/views/frontend/page/home.blade.php
+++ resources/views/frontend/page/home.blade.php
@@ -5,22 +5,28 @@
 
 @section('content')
   <section class="container section">
-    <div class="field">
+    {{--  <div class="field">
       <div class="field-label is-normal">
       </div>
       <div class="control">
         <pencarian kata=""></pencarian>
       </div>
     </div>
-    <p><br></p>
+    <p><br></p>  --}}
     <div class="tile is-ancestor">
       <div class="columns is-multiline">
         @foreach ($posts as $post)
           <div class="tile is-parent is-4">
             <article class="tile is-child box">
-              <p class="title is-5">{{$post->judul}}</p>
+              <p class="title is-5"><a href="/post/{{$post->slug}}">{{$post->judul}}</a></p>
               <p class="subtitle is-6"><small>{{$post->tgl_terbit}}</small></p>
-              <p>{{$post->ringkasan}}</p><a href="/post/{{$post->slug}}">Read more ...</a>
+              <p>{{$post->ringkasan}}</p>
+              <p>
+              <br>
+              @foreach($post->kategoris->pluck('kategori') as $p)
+                <a href="/kategori/{{$p}}"><span class="tag is-light">{{$p}}</span></a>
+              @endforeach
+              </p>
             </article>
           </div>
         @endforeach
