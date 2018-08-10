07f016ee33eae01d46a8c98f3bb6bf8400d7d01e
nusendra <admin@nusendra.com>
2018-08-10 13:59:13 +0700

    add oneSignal push notification

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  @include('frontend.onesignal')
   @include('frontend.ga')
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
---
+++ resources/views/frontend/onesignal.blade.php
@@ -0,0 +1,10 @@
+<link rel="manifest" href="/manifest.json" />
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "62a610f6-a724-45f8-af29-660beb4c2b69",
+    });
+  });
+</script>
