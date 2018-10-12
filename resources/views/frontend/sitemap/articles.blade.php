a706230ab20d8fe8b8cc5d22757b5280a7d3063c
Nusendra Hanggarawan <admin@nusendra.com>
2018-10-12 10:22:09 +0700

    fix sitemap and clear cache

--- resources/views/frontend/sitemap/articles.blade.php
+++ resources/views/frontend/sitemap/articles.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($posts as $article)
         <url>
-            <loc>https://nusendra.com/articles/{{ $article->slug }}</loc>
+            <loc>https://nusendra.com/post/{{ $article->slug }}</loc>
             <lastmod>{{ $article->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
--- resources/views/frontend/sitemap/categories.blade.php
+++ resources/views/frontend/sitemap/categories.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($categories as $category)
         <url>
-            <loc>https://nusendra.com/categories/{{ $category->kategori }}</loc>
+            <loc>https://nusendra.com/kategori/{{ $category->kategori }}</loc>
             <lastmod>{{ $category->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.9</priority>
